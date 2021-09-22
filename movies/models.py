6f2a8673263391025ef73552906972d6045b4f39
hrechykhin <vladgrechikhin@gmail.com>
2021-09-22 22:16:02 +0300

    Static and media

--- movies/models.py
+++ movies/models.py
@@ -103,7 +103,7 @@ class Rating(models.Model):
     """ Ratings """
     ip = models.CharField(verbose_name='IP address', max_length=15)
     star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name='Star')
-    movie = models.ForeignKey(Movie, on_delete=models.CharField, verbose_name='Film')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name='Film')
 
     def __str__(self):
         return f"{self.star} - {self.movie}"
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Movie
+
+class MoviesView(View):
+    """ Films list """
+    def get(self,request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movies.html", {'movie_list':movies})
