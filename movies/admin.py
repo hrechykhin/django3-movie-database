0f6d4f5418dc1c7838e514c5a433dccbbf8b7e1e
hrechykhin <vladgrechikhin@gmail.com>
2021-10-05 22:44:55 +0300

    Actions Settings

--- movies/admin.py
+++ movies/admin.py
@@ -35,6 +35,7 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ("draft",)
+    actions = ["publish", "unpublish"]
     readonly_fields = ("get_image", )
     fieldsets = (
         (None, {
@@ -60,6 +61,30 @@ class MovieAdmin(admin.ModelAdmin):
 
     def get_image(self, obj):
         return mark_safe(f'<img src={obj.poster.url} width="100" height="110">')
+
+    def unpublish(self, request, queryset):
+        """ Unpublish """
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            message_bit = '1 recording was updated'
+        else:
+            message_bit = f"{row_update} recordings were updated"
+        self.message_user(request, f"{message_bit}")
+
+    def publish(self, request, queryset):
+        """ Publish """
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = '1 recording was updated'
+        else:
+            message_bit = f"{row_update} recordings were updated"
+        self.message_user(request, f"{message_bit}")
+
+    publish.short_description = "Publish"
+    publish.allowed_permissions = ('change',)
+
+    unpublish.short_description = "Unpublish"
+    unpublish.allowed_permissions = ('change',)
 
     get_image.short_description = "Poster"
 
