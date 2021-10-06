cc6ff18200e134805450772014314d5639e24fe5
hrechykhin <vladgrechikhin@gmail.com>
2021-10-06 21:41:25 +0300

    Template tags

--- movies/views.py
+++ movies/views.py
@@ -3,13 +3,14 @@ from django.views.generic.base import View
 
 from django.views.generic import ListView, DetailView
 
-from .models import Movie
+from .models import Movie, Category
 from .forms import ReviewForm
 
 class MoviesView(ListView):
     """ Films list """
     model = Movie
     queryset = Movie.objects.filter(draft=False)
+
 
 class MovieDetailView(DetailView):
     """ Description of the film """
